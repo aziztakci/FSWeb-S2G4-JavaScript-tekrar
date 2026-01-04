9d758f47be5358c4837a66d09871a8d5110dee14
aziztakci <takciazizz@gmail.com>
2026-01-04 16:27:30 +0300

    Görev 3.e tamamlandı.

--- index.js
+++ index.js
@@ -141,7 +141,8 @@ console.log(besyuzdenkucuksayilar);
 
 // 3e çözümü
 
-/* kodlar buraya */
+siralisayilar = [...besyuzdenkucuksayilar].sort((a,b) => a-b);
+console.log(siralisayilar);
 
 // 3f çözümü
 
